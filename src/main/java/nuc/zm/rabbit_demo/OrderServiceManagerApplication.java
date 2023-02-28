17347202718a90ddd1670b25a546e83083556eb8
da juancai[2~ <742824624@qq.com>
2023-02-28 21:22:20 +0800

    :bookmark:订单微服务的初始化 并成功运行

---
+++ src/main/java/nuc/zm/rabbit_demo/OrderServiceManagerApplication.java
@@ -0,0 +1,14 @@
+package nuc.zm.rabbit_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceManagerApplication.class, args);
+
+    }
+
+}
